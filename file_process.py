f896941ef41053e6c22b6321b649531bc491cbe5
MakDon <379857334@qq.com>
2017-05-19 00:04:46 +0800

    just push

--- file_process.py
+++ file_process.py
@@ -17,6 +17,7 @@ tf.flags.DEFINE_integer("readlines_num", 512, "The number of output sentences")
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
 def write_file(line,file,counter=0) : 
+    line = line + "\n"
     if FLAGS.num_limits : 
         if counter < sentences_num : 
             file.write(line)
@@ -36,19 +37,26 @@ def classify(file_dir=FLAGS.file_dir,out_dir=FLAGS.out_dir,sentences_num=FLAGS.s
     neg_sentence_counter=0
     read_counter=0
     while 1:
-        lines = input_file.readlines(FLAGS.readlines_num)
-        read_counter = pos_sentence_counter + neg_sentence_counter
-        print("Now read "+format(read_counter) +"sentences\n")
-        if not lines:
-            break
-        isCantonese = predict.predict(lines)
-        for i in range(len(lines)):
-            if isCantonese[i] :
-                pos_sentence_counter += write_file(lines[i],pos_file,pos_sentence_counter)
-            else:
-                neg_sentence_counter += write_file(lines[i],neg_file,neg_sentence_counter)
-        if num_limits :
-            if pos_sentence_counter >= sentences_num and neg_sentence_counter >= sentences_num : break
+        # 先把所有数据读进来，内存应该够用
+        lines = [line.strip() for line in input_file.readlines()]
+
+        pbatch = 1024 # 每次处理1024条
+        iter = 0 # 迭代指针
+
+        while (iter * pbatch) < len(lines):
+            dlines = lines[iter * pbatch : (iter+1) * pbatch]
+            results = predict(dlines)
+            for i in range(len(results)):
+                if results[i] == 1:
+                    # 预测为广东话，把 dlines[i] 写入文件
+                    pos_sentence_counter += write_file(dlines[i],pos_file,pos_sentence_counter)
+                else:
+                    # 预测为普通话，同理
+                    neg_sentence_counter += write_file(dlines[i],neg_file,neg_sentence_counter)
+            if num_limits :
+                if pos_sentence_counter >= sentences_num and neg_sentence_counter >= sentences_num : break
+        iter+=1
+        print("The "+format(pos_sentence_counter+neg_sentence_counter)+" line\n"
     
     pos_file.close()
     neg_file.close()
